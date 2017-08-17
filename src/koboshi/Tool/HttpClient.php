b09137b349a7fbc387c6f60295e376494af4e236
koboshi <koboshi@qq.com>
2017-08-17 23:37:57 +0800

    http client dev

---
+++ src/koboshi/Tool/HttpClient.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Htt客户端工具类
+ * 基于CURL
+ * @author SamDing
+ */
+namespace koboshi\Tool;
+
+class HttpClient
+{
+    private $config = array();
+
+    public static function simpleGet()
+    {
+
+    }
+
+    public static function simpleMultiGet()
+    {
+
+    }
+
+    public static function simplePost()
+    {
+
+    }
+
+    public static function simpleMultiPost()
+    {
+
+    }
+
+    public function __construct()
+    {
+
+    }
+
+    public function url()
+    {
+
+    }
+
+    public function auth()
+    {
+
+    }
+
+    public function userAgent()
+    {
+
+    }
+
+    public function headers()
+    {
+
+    }
+
+    public function proxy()
+    {
+
+    }
+
+    public function cookie()
+    {
+
+    }
+
+    public function method()
+    {
+
+    }
+
+    public function request()
+    {
+
+    }
+
+    public function download(callable $callback, $size = 1024)
+    {
+
+    }
+}
